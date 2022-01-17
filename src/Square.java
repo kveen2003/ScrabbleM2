d7fd8bc79f9582e4939c364619285de34764f698
kveen2003 <k.l.veen@student.utwente.nl>
2022-01-17 12:33:11 +0100

    Certain classes added, certainty input not yet but classes necessary

--- src/Square.java
+++ src/Square.java
@@ -1,5 +1,13 @@
 public class Square {
 
+    public void setTile(Tile tile) {
+    }
+
+    public Square clone(){
+        //implement method here
+        return null;
+    }
+
 //    public Square(int x, int y, Square.type type){
 //
 //    }
---
+++ src/TilesGenerator.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TilesGenerator {
+
+    public static List<Tile> generateTiles(){
+        //List<TileDescription> input = new ArrayList<>();
+        return null;
+
+    }
+
+}
